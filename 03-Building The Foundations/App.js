f19c6f999c580d0712c8758495fb6452abd3a91a
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-13 17:53:05 +0530

    add component composition

--- 03-Building The Foundations/App.js
+++ 03-Building The Foundations/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', {}, 'Hello, World! from React');
+function Title() {
+   return <h1>This is Title</h1>;
+}
+
+function Header() {
+   return (
+      <div>
+         <Title />
+         <h1>This is Header</h1>
+      </div>
+   );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+root.render(<Header />);
