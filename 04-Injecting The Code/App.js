4d383c354606618c5790b9a329bd56f76785b855
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-14 19:36:14 +0530

    add styles for Header

--- 04-Injecting The Code/App.js
+++ 04-Injecting The Code/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const author = 'Harsh Tambe';
-
-function Title() {
-   return <h1>This is Title</h1>;
-}
+import './index.css';
 
 function Header() {
    return (
-      <div>
-         <Title />
-         <h1>This is Header</h1>
-         {author}
+      <>
+         <div className="nav-bar">
+            <h1>Virar Eats</h1>
+
+            <ul>
+               <li>Home</li>
+               <li>About Us</li>
+               <li>Cart</li>
+            </ul>
+         </div>
+
+         <Search />
+      </>
+   );
+}
+
+function Search() {
+   return (
+      <div className="search">
+         <input type="text" placeholder="Search..."></input>
+      </div>
+   );
+}
+
+function App() {
+   return (
+      <div className="app">
+         <Header />
       </div>
    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Header />);
+root.render(<App />);
