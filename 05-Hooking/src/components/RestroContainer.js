64a7bacda2c7adedb10b2af514a6cdafc3b1a944
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-15 18:31:08 +0530

    modify RestroContainer

--- 05-Hooking/src/components/RestroContainer.js
+++ 05-Hooking/src/components/RestroContainer.js
@@ -1,13 +1,17 @@
 import RestroCard from './RestroCard.js';
 import { restroData } from '../utils/constants.js';
+import Filters from './Filters.js';
 
 function RestroContainer() {
    return (
-      <div className="restro-card-container">
-         {restroData.map((restro) => (
-            <RestroCard restroObj={restro} key={restro.info.id} />
-         ))}
-      </div>
+      <>
+         <Filters />
+         <div className="restro-card-container">
+            {restroData.map((restro) => (
+               <RestroCard restroObj={restro} key={restro.info.id} />
+            ))}
+         </div>
+      </>
    );
 }
 
