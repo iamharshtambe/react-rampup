026ecd60abadf54939925cbb676fb205de963b5b
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-17 19:56:06 +0530

    fetch data from api

--- 06-Exploration/src/components/RestroContainer.js
+++ 06-Exploration/src/components/RestroContainer.js
@@ -1,9 +1,25 @@
 import RestroCard from './RestroCard.js';
-import { restroData } from '../utils/constants.js';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function RestroContainer() {
-   const [listOfRestros, setListOfRestros] = useState(restroData);
+   const [listOfRestros, setListOfRestros] = useState([]);
+
+   async function fetchData() {
+      const data = await fetch(
+         'https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.4563596&lng=72.79246119999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+      );
+
+      const json = await data.json();
+
+      setListOfRestros(
+         json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants
+      );
+   }
+
+   useEffect(() => {
+      fetchData();
+   }, []);
 
    function handleClick() {
       const filteredListOfRestros = listOfRestros.filter(
