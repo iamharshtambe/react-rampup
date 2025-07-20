3a781ba058ffdf03a645bc46df4587cbe268ffa9
iamharshtambe <harshtambe.dev@gmail.com>
2025-07-20 16:58:29 +0530

    add lesson classy

---
+++ 08-Classy/src/components/Shimmer.js
@@ -0,0 +1,16 @@
+function Shimmer() {
+   return (
+      <div className="shimmer-ui">
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+         <div className="card"></div>
+      </div>
+   );
+}
+
+export default Shimmer;
